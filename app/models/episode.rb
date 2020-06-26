bf482ec3a8ed4f8b5f440167593aba8dea91de7b
KalebGz <kaleb.zeray@gmail.com>
2020-06-26 11:53:21 -0400

    done

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,9 @@
 class Episode < ApplicationRecord
+    has_many :appearances 
+    has_many :guests, through: :appearances
+
+    def avg_rating
+        appearances.sum(&:rating) / appearances.length
+    end
+        
 end
