15ef7f6fdefdfc0426f0b8d17c3724d835dd4118
robertalopez <roblop1234@yahoo.com>
2020-06-26 08:47:03 -0700

    robert_lopez_mod2

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,9 @@
 class Guest < ApplicationRecord
+has_many :appearances
+has_many :episodes, through: :appearances 
+
+def sorted
+    self.episodes.sort_by{|episode| Appearance.find_by(episode_id: episode.id, guest_id: self.id).rating}.reverse   
+end 
 
 end
