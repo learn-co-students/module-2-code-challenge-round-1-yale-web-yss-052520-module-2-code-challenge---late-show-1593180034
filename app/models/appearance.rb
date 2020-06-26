46214a1019e49575931955d1f35ca0082a817e8c
eric <eric.ou@yale.edu>
2020-06-26 08:59:03 -0700

    done

---
+++ app/models/appearance.rb
@@ -0,0 +1,6 @@
+class  Appearance < ApplicationRecord
+    belongs_to :episode
+    belongs_to :guest
+    validates :rating, inclusion: 1..5
+    validates :guest_id, uniqueness: {scope: :episode_id}
+end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,9 @@
 class Guest < ApplicationRecord
-
+    has_many :appearances
+    has_many :episodes, through: :appearances
+    def sort_by_ratings
+        self.episodes.sort_by{|e|
+            e.appearances.first.rating
+        }
+    end
 end
