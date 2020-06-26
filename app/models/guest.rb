faba71d55e006f1dec00425e9ff1af44853ec136
Joao Cardoso <joao.cardoso@yale.edu>
2020-06-26 11:46:07 -0400

    done

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,8 @@
 class Guest < ApplicationRecord
+    has_many :appearances
+    has_many :episodes, through: :appearances
 
+    def ordered_appearances
+        self.appearances.sort_by{|appearance| appearance.rating}.reverse
+    end
 end
