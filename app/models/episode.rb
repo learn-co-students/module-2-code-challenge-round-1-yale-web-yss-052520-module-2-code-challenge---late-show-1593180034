506454de4a1b9499cdcb1fdecb8c32c56f16b458
Anmolpreet <anmolpreet.kaur@yale.edu>
2020-06-26 11:58:46 -0400

    done

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,12 @@
 class Episode < ApplicationRecord
+    has_many :appearances 
+    has_many :guests, through: :appearances
+
+    def guest_rating(guest_given)
+        appearances.find_by(guest: guest_given).rating
+    end
+
+    def average_rating
+        appearances.average(:rating)
+    end
 end
