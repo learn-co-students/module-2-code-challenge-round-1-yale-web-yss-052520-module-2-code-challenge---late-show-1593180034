d0719f366443e50ce47892ef374dc06b009e3330
justin_james <jjames11799@gmail.com>
2020-06-26 11:14:12 -0400

    server is not working, must restart

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,9 @@
 class Guest < ApplicationRecord
+    has_many :appearances
+    has_many :episodes, through: :appearances 
+
+    def appearances_in_order
+        self.episodes.sort_by{|episode| episode.appearances_average_rating }
+    end
 
 end
