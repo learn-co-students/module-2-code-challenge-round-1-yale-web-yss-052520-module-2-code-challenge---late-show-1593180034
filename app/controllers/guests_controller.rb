bd8956562140fa0b7dc67ddeccf63ee2afb3b756
edensongjbs <edensongjbs@gmail.com>
2020-06-26 11:58:16 -0400

    phew

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,17 @@
 class GuestsController < ApplicationController
+  before_action :current_guest, only: [:show]
 
   def index
     @guests = Guest.all
   end
+
+  def show
+  end
+
+  private
+
+  def current_guest
+    @guest = Guest.find(params[:id])
+  end
+
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,9 @@
 class Guest < ApplicationRecord
+    has_many :appearances
+    has_many :episodes, through: :appearances
+
+    def show
+        @guest = Guest.find(params[:id])
+    end
 
 end
